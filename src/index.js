2587c80b51cf8d962a6d994fd9b2d8c80b8cb2da
vladblkvs <gremlinux@gmail.com>
2020-10-21 20:57:41 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = [];
+    if (matrix) {
+        matrix.forEach((arr, i) => {
+            i % 2 !== 0 ? arr.sort((a, b) => b - a) : arr.sort((a, b) => a - b);
+            arr.forEach(el => {
+                result.push(el);
+            });
+        });
+    }
+    return result;
 }
